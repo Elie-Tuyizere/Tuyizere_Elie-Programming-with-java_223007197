b53888531bba581f4d1d9d3fdf5ea5a6ee96c4e9
Elie-Tuyizere <tuyizereelia333@gmail.com>
2025-09-27 19:36:06 +0200

    Add files via upload

---
+++ Assignment_Indivual_22_09_2025/src/com/TestAll/TestAll.java
@@ -0,0 +1,16 @@
+package com.TestAll;
+
+import com.form.Operations;
+
+public class TestAll {
+
+	
+		public static void main(String[] args) {
+
+			Operations op= new Operations();
+
+
+		
+	}
+
+}
